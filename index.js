3f601856544a490befe375140b84fc3b6988da11
farahhaddar <farah.haddar99@gmail.com>
2020-10-21 22:23:14 +0300

    step 4

--- index.js
+++ index.js
@@ -14,3 +14,37 @@ app.get("/test", function(req, res) {
 app.get("/time", function(req, res) {
     res.send({ status: 200, message: d.getHours() + ":" + d.getSeconds() });
 });
+app.get('/hello/:id', function(req, res) {
+    res.send({
+        status: 200,
+        message: "Hello," +
+            req.params.id
+    });
+});
+app.get('/hello', function(req, res) {
+    res.send({ status: 200, message: "Hello" });
+});
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+
+        const response = {
+            status: 200,
+            message: "ok",
+            data: search
+        };
+
+        res.send(response);
+    } else {
+        const response = {
+            status: 500,
+            error: true,
+            message: "you have to provide a search"
+        };
+
+
+        res.status(500);
+        res.send(response);
+    }
+});
