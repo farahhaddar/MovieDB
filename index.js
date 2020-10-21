e473b021d992cb9d1381babeb9becd79550c5cec
farahhaddar <farah.haddar99@gmail.com>
2020-10-21 21:29:40 +0300

    step 3

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+var d = new Date();
 app.listen(port, function() {
     console.log("Server is running on " + port + " port");
 });
 app.get('/', function(req, res) {
     res.send('ok')
-})
+});
+app.get("/test", function(req, res) {
+    res.send({ status: 200, message: "ok" });
+});
+app.get("/time", function(req, res) {
+    res.send({ status: 200, message: d.getHours() + ":" + d.getSeconds() });
+});
